6a0eb4dd3782ba26bf0ec06d133caa460cf45a42
Alex Cervilla <cervilla.alex@gmail.com>
2019-07-07 20:27:52 +0200

    Version Increment

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='PriveAPI',
-      version='1.2',
+      version='1.3',
       description='API to create programs compatible with Prive',
       keywords='prive',
       url='http://github.com/c3rv1/Prive',
