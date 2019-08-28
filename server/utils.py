9133931c1fa712983ccfd90483e24f84ec7f182a
Alex Cervilla <cervilla.alex@gmail.com>
2019-08-28 14:04:45 +0200

    Utils.py

--- server/utils.py
+++ server/utils.py
@@ -1,6 +1,7 @@
 import inspect
 import base64
 from Crypto.Random import random, get_random_bytes
+import re
 
 
 def lineno():
@@ -20,3 +21,24 @@ def getRandString(len):
     if "\x00" in returnString:
         return getRandString(len)
     return returnString
+
+def extractData(msg):
+        # type: (str) -> tuple
+        """
+
+        Extract Data from Prive Message (data;errorCode: eC)
+
+        Returns ("", "") if it is not a prive message
+
+        :param msg: Message to extract data from
+        :return: data, eC
+        """
+
+        msgRe = re.search("^(.+);errorCode: (.+)", msg)
+        if not msgRe:
+            return "", ""
+
+        msgData = msgRe.group(1)
+        errorCode = msgRe.group(2)
+
+        return msgData, errorCode
