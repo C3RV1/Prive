e21a88751e7c2fde1ca25166f30139564a2a7e9f
Alex Cervilla <cervilla.alex@gmail.com>
2019-09-05 19:39:49 +0200

    Fix config paths

--- server/config.py
+++ server/config.py
@@ -5,8 +5,8 @@ class Config:
     HOST = "127.0.0.1"
     PORT = 4373
     MAX_CURRENT_USERS = 10
-    DATABASE_PATH = "..\\PriveDatabase"
-    LOGFILE = "..\\prive.log"
+    DATABASE_PATH = "../PriveDatabase"
+    LOGFILE = "../prive.log"
     UNACCEPTED_NAME_CHARCTERS = "\r\n/\\\\:\"?*<>|."
     CLIENT_TIMEOUT = 10
     KEYSIZE = 4096
