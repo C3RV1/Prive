0cc17e99f6e35cede4837a68ac1e11865075e837
Alex Cervilla <cervilla.alex@gmail.com>
2019-09-10 11:20:06 +0200

    PriveAPI version change (v2.0)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='PriveAPI',
-      version='1.3',
+      version='2.0',
       description='API to create programs compatible with Prive',
       keywords='prive',
       url='http://github.com/c3rv1/Prive',
